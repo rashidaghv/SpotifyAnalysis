e08d2c71547d2a59637f47f0d6964826b0305cb8
rashidaghv <rashidaghv@gmail.com>
2024-05-27 15:59:08 +0200

    Add secure handling of API credentials using .env

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,9 +1,14 @@
+import os
 import requests
 import pandas as pd
 import concurrent.futures
+from dotenv import load_dotenv
 
+load_dotenv()  # Load environment variables from .env file
 
-def get_spotify_token(client_id, client_secret):
+def get_spotify_token():
+    client_id = os.getenv('SPOTIFY_CLIENT_ID')
+    client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
     url = 'https://accounts.spotify.com/api/token'
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {'grant_type': 'client_credentials'}
@@ -13,7 +18,6 @@ def get_spotify_token(client_id, client_secret):
     else:
         print(f"Failed to get token: {response.text}")
         return None
-
 
 def get_track_url_and_album_cover(data):
     track_name, artist_name, token = data
@@ -26,27 +30,20 @@ def get_track_url_and_album_cover(data):
         return track['external_urls']['spotify'], track['album']['images'][0]['url']
     return None, None
 
-
 # Load your dataset
 file_path = r'C:\Users\ACER\OneDrive\Desktop\Spotify\spotify-2023.csv'
 spotify_data = pd.read_csv(file_path, encoding='ISO-8859-1')
 
 # Authenticate with Spotify
-client_id = '1125f12df57945e98cbe3dfbbcec450f'
-client_secret = 'b118f897702b41718088da90a3334059'
-token = get_spotify_token(client_id, client_secret)
-
+token = get_spotify_token()
 if token:
     # Prepare data for concurrent processing
     data_for_processing = [(row['track_name'], row['artist(s)_name'], token) for index, row in spotify_data.iterrows()]
-
     # Use ThreadPoolExecutor to make requests in parallel
     with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
         results = list(executor.map(get_track_url_and_album_cover, data_for_processing))
-
     # Update dataframe with results
     spotify_data['track_url'], spotify_data['album_cover_url'] = zip(*results)
-
     # Save the enriched dataset
     enriched_file_path = r'C:\Users\ACER\OneDrive\Desktop\Spotify\enriched_spotify_2023.csv'
     spotify_data.to_csv(enriched_file_path, index=False)
